a545611dbc09abfe6420aa45619b4fbd671ac0cf
Sinstraliz <blagovest.vp@gmail.com>
2016-04-26 17:06:02 +0300

    Fixed build.cmd, .gitignore and removed AssemblyInfo.cs

--- build.cmd
+++ build.cmd
@@ -2,35 +2,52 @@
 
 SETLOCAL
 
-SET TOOLS_PATH=.\bin\tools
-SET NUGET=%TOOLS_PATH%\NuGet\NuGet.exe
-SET FAKE=%TOOLS_PATH%\FAKE\tools\Fake.exe
-SET NYX=%TOOLS_PATH%\Nyx\tools\build.fsx
-SET BUILD_TOOLS_PATH="%ProgramFiles(x86)%\MSBuild\12.0\bin\MSBuild.exe"
+SET NUGET=%LocalAppData%\NuGet\NuGet.exe
+@echo off
 
-IF NOT EXIST %BUILD_TOOLS_PATH% (
-  echo In order to build or run this tool you need either Visual Studio 2015 or
-  echo Microsoft Build Tools 2013 tools installed.
+SETLOCAL
+
+SET NUGET=%LocalAppData%\NuGet\NuGet.exe
+SET FAKE=%LocalAppData%\FAKE\tools\Fake.exe
+SET NYX=%LocalAppData%\Nyx\tools\build.fsx
+SET GITVERSION=%LocalAppData%\GitVersion.CommandLine\tools\GitVersion.exe
+SET MSBUILD14_TOOLS_PATH="%ProgramFiles(x86)%\MSBuild\14.0\bin\MSBuild.exe"
+SET MSBUILD12_TOOLS_PATH="%ProgramFiles(x86)%\MSBuild\12.0\bin\MSBuild.exe"
+SET BUILD_TOOLS_PATH=%MSBUILD14_TOOLS_PATH%
+
+IF NOT EXIST %MSBUILD14_TOOLS_PATH% (
+  echo In order to run this tool you need either Visual Studio 2015 or
+  echo Microsoft Build Tools 2015 tools installed.
   echo.
   echo Visit this page to download either:
   echo.
-  echo http://www.microsoft.com/en-us/download/details.aspx?id=40760
+  echo http://www.visualstudio.com/en-us/downloads/visual-studio-2015-downloads-vs
   echo.
-  goto :eof
+  echo Attempting to fall back to MSBuild 12 for building only
+  echo.
+  IF NOT EXIST %MSBUILD12_TOOLS_PATH% (
+    echo Could not find MSBuild 12.  Please install build tools ^(See above^)
+    exit /b 1
+  ) else (
+    set BUILD_TOOLS_PATH=%MSBUILD12_TOOLS_PATH%
+  )
 )
 
 echo Downloading latest version of NuGet.exe...
-IF NOT EXIST %TOOLS_PATH%\NuGet md %TOOLS_PATH%\NuGet
+IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%NUGET%'"
 
-echo Downloading latest version of Fake.exe...
-%NUGET% "install" "FAKE" "-OutputDirectory" "%TOOLS_PATH%" "-ExcludeVersion" "-Prerelease"
+echo Downloading latest version of FAKE...
+IF NOT EXIST %LocalAppData%\FAKE %NUGET% "install" "FAKE" "-OutputDirectory" "%LocalAppData%" "-ExcludeVersion" "-Version" "4.4.4"
 
-echo Downloading latest version of Nuget.Core...
-%NUGET% "install" "Nuget.Core" "-OutputDirectory" "%TOOLS_PATH%" "-ExcludeVersion" "-Prerelease"
+echo Downloading latest version of NuGet.Core...
+IF NOT EXIST %LocalAppData%\NuGet.Core %NUGET% "install" "NuGet.Core" "-OutputDirectory" "%LocalAppData%" "-ExcludeVersion" "-Version" "2.8.6"
+
+echo Downloading latest version of GitVersion.CommandLine...
+IF NOT EXIST %LocalAppData%\GitVersion.CommandLine %NUGET% "install" "GitVersion.CommandLine" "-OutputDirectory" "%LocalAppData%" "-ExcludeVersion" "-Version" "3.3.0"
 
 echo Downloading latest version of Nyx...
-%NUGET% "install" "Nyx" "-OutputDirectory" "%TOOLS_PATH%" "-ExcludeVersion" "-Prerelease"
+%NUGET% "install" "Nyx" "-OutputDirectory" "%LocalAppData%" "-ExcludeVersion" "-PreRelease"
 
 SET TARGET="Build"
 
