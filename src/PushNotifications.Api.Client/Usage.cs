e229cee0d32ae5f749d3d7cce026c4464f3cd5b1
SayliS <kalin.venkov@eldersoss.com>
2017-11-14 17:00:08 +0200

    Updates packages

--- src/PushNotifications.Api.Client/Usage.cs
+++ src/PushNotifications.Api.Client/Usage.cs
@@ -10,17 +10,27 @@ namespace PushNotifications.Api.Client
             Uri authority = new Uri("https://account-vapt.local.com");
             Uri apiAddress = new Uri("https://pn-new.local.com");
 
+            Uri qoreAuthority = new Uri("https://account-qore.local.com");
+
             var pnClient = new PushNotificationsRestClient(authority, "connect/token", "localclient", "localsecret", "read write admin", apiAddress);
+            var pnClientForMarketVision = new PushNotificationsRestClient(qoreAuthority, "connect/token", "localclient", "localsecret", "read write admin", apiAddress);
 
             var t = Timestamp.JudgementDay();
-            for (int i = 0; i < 10; i++)
-            {
-                var sent = pnClient.SendPushNotification(new Models.PushNotificationSendModel("pruvit", new Contracts.SubscriberId(i.ToString(), "pruvit"), $"titleee", "bodyy", "default", string.Empty, 0, t, true));
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    var sent = pnClient.SendPushNotification(new Models.PushNotificationSendModel("pruvit", new Contracts.SubscriberId(i.ToString(), "pruvit"), $"titleee", "bodyy", "default", string.Empty, 0, t, true));
 
-                //var x = pnClient.SubscribeForFireBase(new SubscribeForFireBaseModel("pruvit", new SubscriberId(i.ToString(), "pruvit").Urn.Value, new SubscriptionToken($"token-{i}")));
-            }
+            //    //var x = pnClient.SubscribeForFireBase(new SubscribeForFireBaseModel("pruvit", new SubscriberId(i.ToString(), "pruvit").Urn.Value, new SubscriptionToken($"token-{i}")));
+            //}
 
-            var xx = pnClient.SendPushNotification(new Models.PushNotificationSendModel("pruvit", new Contracts.SubscriberId("76277", "pruvit"), $"titleee", "bodyy", "default", string.Empty, 0, t, true));
+            var pruvitSubscriber = new Contracts.SubscriberId("76277", "pruvit");
+            var pruvitSubscribe = pnClient.SubscribeForFireBase(new SubscribeForFireBaseModel("pruvit", pruvitSubscriber.Urn.Value, new SubscriptionToken("exk_tnns_OI:APA91bFzlzvFysNlYpVmvdBbT1gVS66PeJ8izHLGwO0i3nu8TZGYBQrorURNM_fu2tPupGi9Zm1k-u1wIjwwclqdz-9IGjxbtqLa4PlH2B8h6T4xr1JMULKJbVeaZEcGJO1K2_0tmBxd")));
+            var x = pnClient.SendPushNotification(new Models.PushNotificationSendModel("pruvit", new Contracts.SubscriberId("76277", "pruvit"), $"titleee", "bodyy", "default", string.Empty, 0, t, true));
+
+
+            var mvSubscriber = new Contracts.SubscriberId("76277", "mv");
+            var s = pnClient.SubscribeForFireBase(new SubscribeForFireBaseModel("mv", mvSubscriber.Urn.Value, new SubscriptionToken("exk_tnns_OI:APA91bFzlzvFysNlYpVmvdBbT1gVS66PeJ8izHLGwO0i3nu8TZGYBQrorURNM_fu2tPupGi9Zm1k-u1wIjwwclqdz-9IGjxbtqLa4PlH2B8h6T4xr1JMULKJbVeaZEcGJO1K2_0tmBxd")));
+            var xx = pnClientForMarketVision.SendPushNotification(new Models.PushNotificationSendModel("mv", mvSubscriber, $"titleee - MV", "bodyy", "default", string.Empty, 0, t, true));
         }
     }
 }
