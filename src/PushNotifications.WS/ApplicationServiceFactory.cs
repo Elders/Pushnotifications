424c87aa431905a277df7abfcf53edb0b06b74bc
SayliS <kalin.venkov@eldersoss.com>
2017-11-22 11:49:22 +0200

    Adds null check for ApplicationServiceFactory

--- src/PushNotifications.WS/ApplicationServiceFactory.cs
+++ src/PushNotifications.WS/ApplicationServiceFactory.cs
@@ -12,12 +12,17 @@ namespace PushNotifications.WS
 
         public ApplicationServiceFactory(IContainer container, string namedInstance)
         {
+            if (ReferenceEquals(null, container) == true) throw new ArgumentNullException(nameof(container));
+            if (string.IsNullOrEmpty(namedInstance) == true) throw new ArgumentNullException(nameof(namedInstance));
+
             this.container = container;
             this.namedInstance = namedInstance;
         }
 
         public object Create(Type appServiceType)
         {
+            if (ReferenceEquals(null, appServiceType) == true) throw new ArgumentNullException(nameof(appServiceType));
+
             var appService = FastActivator
                 .CreateInstance(appServiceType)
                 .AssignPropertySafely<IAggregateRootApplicationService>(x => x.Repository = container.Resolve<IAggregateRepository>(namedInstance));
