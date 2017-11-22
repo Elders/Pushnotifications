a5dd52bb8f4e5a05788998eded4a3ffb39770d54
SayliS <kalin.venkov@eldersoss.com>
2017-11-22 11:46:51 +0200

    Adds null check for FireBaseSendNotificationModel

--- src/PushNotifications.Delivery.FireBase/Models/FireBaseSendNotificationModel.cs
+++ src/PushNotifications.Delivery.FireBase/Models/FireBaseSendNotificationModel.cs
@@ -1,9 +1,14 @@
-﻿namespace PushNotifications.Delivery.FireBase
+﻿using System;
+
+namespace PushNotifications.Delivery.FireBase
 {
     public class FireBaseSendNotificationModel
     {
         public FireBaseSendNotificationModel(string title, string body, string sound, string badge)
         {
+            if (string.IsNullOrEmpty(title) == true) throw new ArgumentNullException(nameof(title));
+            if (string.IsNullOrEmpty(body) == true) throw new ArgumentNullException(nameof(body));
+
             Title = title;
             Body = body;
             Sound = sound;
