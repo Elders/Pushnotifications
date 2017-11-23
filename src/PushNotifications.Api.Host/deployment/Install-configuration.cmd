7dcfef14ab8aa288936bc0880d337457bdb208ff
SayliS <kalin.venkov@eldersoss.com>
2017-11-23 23:11:01 +0200

    Adds deployment scripts

---
+++ src/PushNotifications.Api.Host/deployment/Install-configuration.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+SET WEBSITE_SOURCE=%~dp0..\content
+
+powershell  -executionPolicy bypass -Command "& '.\.nyx\setup-website-webapp.ps1' -Tenant %1 -Website %2 -WebsiteSource '%WEBSITE_SOURCE%' -Company 'Elders' -App 'push-notifications' -AppHost 'api'"
