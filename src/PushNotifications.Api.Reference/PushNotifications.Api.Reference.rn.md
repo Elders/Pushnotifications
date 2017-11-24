f36e80dd53c76db474f480afad285aa1a3249209
SayliS <kalin.venkov@eldersoss.com>
2017-11-24 18:05:35 +0200

    Updates deployment of the documentation

--- src/PushNotifications.Api.Reference/PushNotifications.Api.Reference.rn.md
+++ src/PushNotifications.Api.Reference/PushNotifications.Api.Reference.rn.md
@@ -1,3 +1,6 @@
+#### 3.0.0-beta0002 - 24.11.2017
+* Updates deployment
+
 #### 3.0.0-beta0001 - 22.11.2017
 * Initial release for version 3 of pn
 
--- src/PushNotifications.Api.Reference/deployment/Install-configuration.cmd
+++ src/PushNotifications.Api.Reference/deployment/Install-configuration.cmd
@@ -2,4 +2,4 @@
 
 SET WEBSITE_SOURCE=%~dp0..\content
 
-powershell  -executionPolicy bypass -Command "& '.\.nyx\setup-website-webapp.ps1' -Tenant %1 -Website %2 -WebsiteSource '%WEBSITE_SOURCE%' -Company 'Elders' -App 'push-notifications' -AppHost 'api/reference'"
+powershell  -executionPolicy bypass -Command "& '.\.nyx\setup-website-webapp.ps1' -Tenant %1 -Website %2 -WebsiteSource '%WEBSITE_SOURCE%' -Company 'Elders' -App 'push-notifications' -AppHost 'reference'"
