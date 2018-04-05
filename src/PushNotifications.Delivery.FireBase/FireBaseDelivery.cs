7de72d61bfe92aac00d76397e2bb394b7bb375ff
mynkow <mynkow@gmail.com>
2018-04-05 09:18:03 +0300

    Removes ContentAvailable which makes the pushnotification to be a silent pushnotification. Apple/Google throttle or drop some of those when there are many sent to a device because it may drain battery on the device. Instead we use the `priority` set to `high`.

--- src/PushNotifications.Delivery.FireBase/FireBaseDelivery.cs
+++ src/PushNotifications.Delivery.FireBase/FireBaseDelivery.cs
@@ -63,7 +63,7 @@ namespace PushNotifications.Delivery.FireBase
             var payload = notification.NotificationPayload;
             var data = notification.NotificationData;
             var fireBaseSendNotificationModel = new FireBaseSendNotificationModel(payload.Title, payload.Body, payload.Sound, payload.Badge.ToString());
-            var model = new FireBaseSendModel(tokensAsStrings, fireBaseSendNotificationModel, data, notification.ExpiresAt, notification.ContentAvailable);
+            var model = new FireBaseSendModel(tokensAsStrings, fireBaseSendNotificationModel, data, notification.ExpiresAt);
             var request = CreateRestRequest(resource, Method.POST).AddJsonBody(model);
             var result = restClient.Execute<FireBaseResponseModel>(request);
 
@@ -74,7 +74,7 @@ namespace PushNotifications.Delivery.FireBase
                 return false;
             }
 
-            log.Info($"[FireBase] success: pn with body {notification.NotificationPayload?.Body} was sent to {tokens.Count} tokens");
+            log.Info($"[FireBase] success: PN with body {notification.NotificationPayload?.Body} was sent to {tokens.Count} tokens");
             return true;
         }
 
