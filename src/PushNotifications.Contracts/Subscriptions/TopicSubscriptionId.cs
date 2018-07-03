bf0a1455c7fc9d366102e42f1250e0ac49cfdb30
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-03 17:07:54 +0300

    Adds Contracts for TopicSubscription aggregate

---
+++ src/PushNotifications.Contracts/Subscriptions/TopicSubscriptionId.cs
@@ -0,0 +1,15 @@
+﻿using Elders.Cronus;
+using System.Runtime.Serialization;
+
+namespace PushNotifications.Contracts.Subscriptions
+{
+    [DataContract(Name = "541f8599-28ff-470d-a628-33f68d7178bc")]
+    public class TopicSubscriptionId : StringTenantId
+    {
+        TopicSubscriptionId() { }
+
+        public TopicSubscriptionId(IUrn urn) : base(urn, "topicSubscription") { }
+
+        public TopicSubscriptionId(string id, string tenant) : base(id, "topicSubscription", tenant) { }
+    }
+}
