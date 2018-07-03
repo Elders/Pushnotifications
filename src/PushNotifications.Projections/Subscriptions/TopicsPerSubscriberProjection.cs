e559135e5922947ab90bf13d01ce3a01b4e3f414
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-03 17:07:04 +0300

    Adds TopicsPerSubscriberProjection

---
+++ src/PushNotifications.Projections/Subscriptions/TopicsPerSubscriberProjection.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+using Elders.Cronus;
+using Elders.Cronus.Projections;
+using PushNotifications.Contracts;
+using PushNotifications.Contracts.Subscriptions.Events;
+
+namespace PushNotifications.Projections.Subscriptions
+{
+    [DataContract(Name = "fc35e537-f06b-406b-b6c3-94725853a278")]
+    public class TopicsPerSubscriberProjection : ProjectionDefinition<SubscriberTopics, SubscriberId>, IProjection,
+       IEventHandler<SubscribedToTopic>,
+       IEventHandler<UnsubscribedFromTopic>
+    {
+        public TopicsPerSubscriberProjection()
+        {
+            Subscribe<SubscribedToTopic>(x => x.SubscriberId);
+            Subscribe<UnsubscribedFromTopic>(x => x.SubscriberId);
+        }
+
+        public void Handle(SubscribedToTopic @event)
+        {
+            State.SubscriberId = @event.SubscriberId;
+            State.Topics.Add(@event.Topic);
+        }
+
+        public void Handle(UnsubscribedFromTopic @event)
+        {
+            State.SubscriberId = @event.SubscriberId;
+            State.Topics.Remove(@event.Topic);
+        }
+    }
+}
