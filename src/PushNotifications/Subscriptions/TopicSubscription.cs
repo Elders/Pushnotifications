db3baaf98d5aa9548831fbbab7578c4c1aa1228c
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-03 17:05:11 +0300

    Adds TopicSubscription aggregate with needed infrastructure

---
+++ src/PushNotifications/Subscriptions/TopicSubscription.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Elders.Cronus;
+using PushNotifications.Contracts;
+using PushNotifications.Contracts.Subscriptions.Events;
+
+namespace PushNotifications.Subscriptions
+{
+    public class TopicSubscription : AggregateRoot<TopicSubscriptionState>
+    {
+        public TopicSubscription(SubscriberId subscriberId, Topic topic)
+        {
+            if (StringTenantId.IsValid(subscriberId) == false) throw new ArgumentException(nameof(subscriberId));
+            if (ReferenceEquals(null, topic)) throw new ArgumentNullException(nameof(topic));
+
+            state = new TopicSubscriptionState();
+
+            IEvent evnt = new SubscribedToTopic(subscriberId, topic);
+            Apply(evnt);
+        }
+
+        public void SubscribeToTopic(SubscriberId subscriberId, Topic topic)
+        {
+            if (StringTenantId.IsValid(subscriberId) == false) throw new ArgumentException(nameof(subscriberId));
+            if (ReferenceEquals(null, topic)) throw new ArgumentNullException(nameof(topic));
+
+            if (state.IsSubscriptionActive == false || state.SubscriberId != subscriberId)
+            {
+                IEvent evnt = new SubscribedToTopic(subscriberId, topic);
+                Apply(evnt);
+            }
+        }
+
+        public void UnsubscribeFromTopic(SubscriberId subscriberId, Topic topic)
+        {
+            if (StringTenantId.IsValid(subscriberId) == false) throw new ArgumentException(nameof(subscriberId));
+            if (ReferenceEquals(null, topic)) throw new ArgumentNullException(nameof(topic));
+
+            if (state.IsSubscriptionActive)
+            {
+                IEvent evnt = new UnsubscribedFromTopic(subscriberId, topic);
+                Apply(evnt);
+            }
+        }
+    }
+}
