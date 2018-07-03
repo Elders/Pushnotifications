b61962e39193de9624cfca7581dfd6adf2ff6e50
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-03 17:05:11 +0300

    Adds TopicSubscription aggregate with needed infrastructure

---
+++ src/PushNotifications/Subscriptions/TopicSubscriptionAppService.cs
@@ -0,0 +1,33 @@
+﻿using Elders.Cronus;
+using PushNotifications.Contracts.Subscriptions.Commands;
+
+namespace PushNotifications.Subscriptions
+{
+    public class TopicSubscriptionAppService : AggregateRootApplicationService<TopicSubscription>,
+        ICommandHandler<SubscribeToTopic>,
+        ICommandHandler<UnsubscribeFromTopic>
+    {
+        public void Handle(SubscribeToTopic command)
+        {
+            TopicSubscription topicSubscription;
+            if (Repository.TryLoad(command.Id, out topicSubscription))
+            {
+                Update(command.Id, x => x.SubscribeToTopic(command.SubscriberId, command.Topic));
+            }
+            else
+            {
+                topicSubscription = new TopicSubscription(command.SubscriberId, command.Topic);
+                Repository.Save<TopicSubscription>(topicSubscription);
+            }
+        }
+
+        public void Handle(UnsubscribeFromTopic command)
+        {
+            TopicSubscription topicSubscription;
+            if (Repository.TryLoad(command.Id, out topicSubscription))
+            {
+                Update(command.Id, x => x.UnsubscribeFromTopic(command.SubscriberId, command.Topic));
+            }
+        }
+    }
+}
---
+++ src/PushNotifications/Subscriptions/TopicSubscriptionState.cs
@@ -0,0 +1,30 @@
+﻿using Elders.Cronus;
+using PushNotifications.Contracts;
+using PushNotifications.Contracts.Subscriptions;
+using PushNotifications.Contracts.Subscriptions.Events;
+
+namespace PushNotifications.Subscriptions
+{
+    public class TopicSubscriptionState : AggregateRootState<TopicSubscription, TopicSubscriptionId>
+    {
+        public override TopicSubscriptionId Id { get; set; }
+
+        public SubscriberId SubscriberId { get; set; }
+
+        public Topic Topic { get; set; }
+
+        public bool IsSubscriptionActive { get; set; }
+
+        public void When(SubscribedToTopic e)
+        {
+            SubscriberId = e.SubscriberId;
+            Topic = e.Topic;
+            IsSubscriptionActive = true;
+        }
+
+        public void When(UnsubscribedFromTopic e)
+        {
+            IsSubscriptionActive = false;
+        }
+    }
+}
