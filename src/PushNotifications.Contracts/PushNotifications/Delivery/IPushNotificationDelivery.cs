de0c0edeb36dc93d819ad4d8cfc13f2df8775fad
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-06 10:44:48 +0300

    Adds an interface to manage Topic Subscriptions

--- src/PushNotifications.Contracts/PushNotifications/Delivery/IPushNotificationDelivery.cs
+++ src/PushNotifications.Contracts/PushNotifications/Delivery/IPushNotificationDelivery.cs
@@ -3,5 +3,14 @@
     public interface IPushNotificationDelivery
     {
         bool Send(SubscriptionToken token, NotificationForDelivery notification);
+
+        bool SendToTopic(Topic topic, NotificationForDelivery notification);
+    }
+
+    public interface ITopicSubscriptionManager
+    {
+        bool SubscribeToTopic(SubscriptionToken token, Topic topic);
+
+        bool UnsubscribeFromTopic(SubscriptionToken token, Topic topic);
     }
 }
