8b927d3236073a9bc71793bd49aa1dbbd01eddb9
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-06 10:48:36 +0300

    Extends PandoraMultiTenantDeliveryProvisioner to be able to provision TopicSubscriptionManagers

---
+++ src/Multitenancy.Delivery/MultiTenantStoreSubscriptionItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Elders.Cronus;
+using PushNotifications.Contracts.PushNotifications.Delivery;
+using PushNotifications.Contracts.Subscriptions;
+
+namespace Multitenancy.Delivery
+{
+    public class MultiTenantStoreSubscriptionItem : ValueObject<MultiTenantStoreItem>
+    {
+        public MultiTenantStoreSubscriptionItem(string tenant, SubscriptionType subscriptionType, ITopicSubscriptionManager topicSubscriptionManager)
+        {
+            if (string.IsNullOrEmpty(tenant) == true) throw new ArgumentNullException(nameof(tenant));
+            if (ReferenceEquals(null, subscriptionType) == true) throw new ArgumentNullException(nameof(subscriptionType));
+            if (ReferenceEquals(null, topicSubscriptionManager) == true) throw new ArgumentNullException(nameof(topicSubscriptionManager));
+
+            Tenant = tenant;
+            SubscriptionType = subscriptionType;
+            TopicSubscriptionManager = topicSubscriptionManager;
+        }
+
+        public string Tenant { get; private set; }
+
+        public SubscriptionType SubscriptionType { get; private set; }
+
+        public ITopicSubscriptionManager TopicSubscriptionManager { get; private set; }
+    }
+}
