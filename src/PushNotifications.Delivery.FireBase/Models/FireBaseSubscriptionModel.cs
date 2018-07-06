2a1e96d27a163fce3819d50f872b4fbf5e46518b
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-06 10:46:55 +0300

    Adds FireBaseTopicSubscriptionManager and models for it

---
+++ src/PushNotifications.Delivery.FireBase/Models/FireBaseSubscriptionModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using PushNotifications.Contracts;
+
+namespace PushNotifications.Delivery.FireBase.Models
+{
+    public class FireBaseSubscriptionModel
+    {
+        public FireBaseSubscriptionModel(List<string> registrationTokens, Topic topic)
+        {
+            if (ReferenceEquals(null, registrationTokens)) throw new ArgumentNullException(nameof(registrationTokens));
+            if (ReferenceEquals(null, topic)) throw new ArgumentNullException(nameof(topic));
+
+            RegistrationTokens = registrationTokens;
+            Topic = "/topics/" + topic.Value;
+        }
+
+        public FireBaseSubscriptionModel(string registrationToken, Topic topic)
+        {
+            if (string.IsNullOrEmpty(registrationToken)) throw new ArgumentNullException(nameof(registrationToken));
+            if (ReferenceEquals(null, topic)) throw new ArgumentNullException(nameof(topic));
+
+            RegistrationTokens = new List<string>() { registrationToken };
+            Topic = "/topics/" + topic.Value;
+        }
+
+        [JsonProperty(PropertyName = "registration_tokens")]
+        public List<string> RegistrationTokens { get; private set; }
+
+        [JsonProperty(PropertyName = "to")]
+        public string Topic { get; private set; }
+    }
+}
