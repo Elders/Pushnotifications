d888d860ef2903f8771d8135c5e5a8b72df4e4cb
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-09 10:53:01 +0300

    Adds Commands and Events for TopicSendPushNotification

---
+++ src/PushNotifications.Contracts/PushNotifications/TopicPushNotificationId.cs
@@ -0,0 +1,15 @@
+﻿using Elders.Cronus;
+using System.Runtime.Serialization;
+
+namespace PushNotifications.Contracts.PushNotifications
+{
+    [DataContract(Name = "a798892a-0d80-401c-aa42-d43f7d5634ce")]
+    public class TopicPushNotificationId : StringTenantId
+    {
+        TopicPushNotificationId() { }
+
+        public TopicPushNotificationId(IUrn urn) : base(urn, "topicpushnotification") { }
+
+        public TopicPushNotificationId(string id, string tenant) : base(id, "topicpushnotification", tenant) { }
+    }
+}
