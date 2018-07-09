afa225d8d707c8c3705c633da518ca77e1ac33fd
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-09 11:40:07 +0300

    Adds a way to GetDeliveryProviders from IDeliveryProvisioners

--- src/Multitenancy.Delivery/IDeliveryProvisioner.cs
+++ src/Multitenancy.Delivery/IDeliveryProvisioner.cs
@@ -1,15 +1,13 @@
 ﻿using PushNotifications.Contracts.PushNotifications.Delivery;
 using PushNotifications.Contracts.Subscriptions;
+using System.Collections.Generic;
 
 namespace Multitenancy.Delivery
 {
     public interface IDeliveryProvisioner
     {
         IPushNotificationDelivery ResolveDelivery(SubscriptionType subscriptionType, NotificationForDelivery notification);
-    }
 
-    public interface ITopicSubscriptionProvisioner
-    {
-        ITopicSubscriptionManager ResolveTopicSubscriptionManager(SubscriptionType subscriptionType, string tenant);
+        IEnumerable<IPushNotificationDelivery> GetDeliveryProviders(string tenant);
     }
 }
