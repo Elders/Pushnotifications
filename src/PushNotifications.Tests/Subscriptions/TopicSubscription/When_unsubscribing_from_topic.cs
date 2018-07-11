48841bd4c0452d5ea640ad2c32ad0fef52ddaf9a
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-11 16:49:42 +0300

    Adds Unit tests

---
+++ src/PushNotifications.Tests/Subscriptions/TopicSubscription/When_unsubscribing_from_topic.cs
@@ -0,0 +1,37 @@
+﻿using Machine.Specifications;
+using PushNotifications.Contracts;
+using PushNotifications.Contracts.Subscriptions;
+using PushNotifications.Subscriptions;
+
+namespace PushNotifications.Tests.PushNotifications
+{
+    [Subject(nameof(TopicSubscription))]
+    public class When_unsubscribing_from_topic
+    {
+        Establish context = () =>
+        {
+            var tenant = "elders";
+            topic = new Topic("topic");
+            subscriberId = new SubscriberId("id", tenant);
+            topicSubscriptionId = new TopicSubscriptionId(subscriberId, topic, tenant);
+            subscriptionType = SubscriptionType.FireBase;
+            aggregate = new TopicSubscription(topicSubscriptionId, subscriptionType);
+        };
+
+        Because of = () => aggregate.UnsubscribeFromTopic(topicSubscriptionId, subscriptionType);
+
+        It should = () => aggregate.RootState().IsSubscriptionActive.ShouldBeFalse();
+
+        It should_have_the_same_topic = () => aggregate.RootState().Id.Topic.ShouldEqual(topic);
+
+        It should_have_the_same_subscriber_id = () => aggregate.RootState().Id.SubscriberId.ShouldEqual(subscriberId);
+
+        It should_have_the_same_topic_subscription_id = () => aggregate.RootState().Id.ShouldEqual(topicSubscriptionId);
+
+        static TopicSubscription aggregate;
+        static TopicSubscriptionId topicSubscriptionId;
+        static SubscriberId subscriberId;
+        static SubscriptionType subscriptionType;
+        static Topic topic;
+    }
+}
