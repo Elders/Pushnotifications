47e6cb63b8f7a0a62992e4fed29668d126441516
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-11 16:51:03 +0300

    Adds Projections Tests

---
+++ src/PushNotifications.Projections.Tests/TopicSubscriptions/When_subscriber_has_subscribed_to_topic_twice.cs
@@ -0,0 +1,30 @@
+﻿using Machine.Specifications;
+using PushNotifications.Contracts;
+using PushNotifications.Contracts.Subscriptions;
+using PushNotifications.Contracts.Subscriptions.Events;
+using PushNotifications.Projections.Subscriptions;
+
+namespace PushNotifications.Tests.PushNotifications
+{
+    [Subject(nameof(TopicsPerSubscriberProjection))]
+    public class When_subscriber_has_subscribed_to_topic_twice
+    {
+        Establish context = () =>
+        {
+            var topic = new Topic("topic");
+            var subscriberId = new SubscriberId("kv", "elders");
+            var id = new TopicSubscriptionId(subscriberId, topic, "elders");
+
+            projection = new TopicsPerSubscriberProjection();
+            @event = new SubscribedToTopic(id, SubscriptionType.FireBase);
+            projection.Handle(@event);
+        };
+
+        Because of = () => projection.Handle(@event);
+
+        It should_have_exactly_one_topic_subscription = () => projection.State.Topics.Count.ShouldEqual(1);
+
+        static TopicsPerSubscriberProjection projection;
+        static SubscribedToTopic @event;
+    }
+}
---
+++ src/PushNotifications.Projections.Tests/TopicSubscriptions/When_subscriber_has_unsubscribed_from_topic_twice.cs
@@ -0,0 +1,38 @@
+﻿using Machine.Specifications;
+using PushNotifications.Contracts;
+using PushNotifications.Contracts.Subscriptions;
+using PushNotifications.Contracts.Subscriptions.Events;
+using PushNotifications.Projections.Subscriptions;
+
+namespace PushNotifications.Tests.PushNotifications
+{
+    [Subject(nameof(TopicsPerSubscriberProjection))]
+    public class When_subscriber_has_unsubscribed_from_topic_twice
+    {
+        Establish context = () =>
+        {
+            topic = new Topic("topic");
+            subscriberId = new SubscriberId("kv", "elders");
+            var subscriptionType = SubscriptionType.FireBase;
+            var id = new TopicSubscriptionId(subscriberId, topic, "elders");
+
+            projection = new TopicsPerSubscriberProjection();
+            @event = new SubscribedToTopic(id, subscriptionType);
+            projection.Handle(@event);
+
+            unsubscribeEvent = new UnsubscribedFromTopic(id, subscriptionType);
+            projection.Handle(unsubscribeEvent);
+        };
+
+        Because of = () => projection.Handle(unsubscribeEvent);
+
+        It should_have_correct_id = () => projection.State.SubscriberId.ShouldEqual(subscriberId);
+        It should_not_have_any_topics_registered = () => projection.State.Topics.ShouldBeEmpty();
+
+        static TopicsPerSubscriberProjection projection;
+        static SubscriberId subscriberId;
+        static Topic topic;
+        static SubscribedToTopic @event;
+        static UnsubscribedFromTopic unsubscribeEvent;
+    }
+}
