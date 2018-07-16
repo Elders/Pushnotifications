5fedbf384aea2a09d632e605159b455f635114ca
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-16 16:39:44 +0300

    Adds Unit tests for the FireBaseDelivery

---
+++ src/PushNotifications.Delivery.FireBase.Tests/When_doing_something.cs
@@ -0,0 +1,50 @@
+﻿using Machine.Specifications;
+using PushNotifications.Contracts;
+using PushNotifications.Contracts.Subscriptions;
+using PushNotifications.Delivery.FireBase.Models;
+using System.Collections.Generic;
+using static PushNotifications.Delivery.FireBase.FireBaseDelivery;
+
+namespace PushNotifications.Delivery.FireBase.Tests
+{
+    [Subject(nameof(FireBaseDelivery))]
+    public class When_doing_something
+    {
+        Establish context = () =>
+        {
+            subscriptionType = SubscriptionType.FireBase;
+            notWorkingSubscriptionToken = new SubscriptionToken("firebase_subscription_token_not_working", subscriptionType);
+            workingSubscriptionToken = new SubscriptionToken("firebase_subscription_token_working", subscriptionType);
+
+            var notRegisteredError = new FireBaseResponseModel.FireBaseResponseResultModel() { Error = "NotRegistered" };
+            var successfullToken = new FireBaseResponseModel.FireBaseResponseResultModel() { Error = "id_123123" };
+
+            responseFromFirebase = new List<FireBaseResponseModel.FireBaseResponseResultModel>()
+            {
+                notRegisteredError, successfullToken,
+                notRegisteredError, successfullToken
+            };
+
+            subscriptionTokens = new List<SubscriptionToken>()
+            {
+                notWorkingSubscriptionToken, workingSubscriptionToken,
+                notWorkingSubscriptionToken, workingSubscriptionToken
+            };
+        };
+
+        Because of = () => result = ExpiredTokensDetector.GetNotRegisteredTokens(subscriptionTokens, responseFromFirebase);
+
+        It should_have_failed_tokens = () => result.HasFailedTokens.ShouldBeTrue();
+
+        It should_have_two_failed_tokens = () => result.FailedTokens.ShouldContainOnly(notWorkingSubscriptionToken, notWorkingSubscriptionToken);
+
+        It should_have_failed_tokens_not_empty = () => result.FailedTokens.ShouldNotBeEmpty();
+
+        private static SubscriptionType subscriptionType;
+        private static List<FireBaseResponseModel.FireBaseResponseResultModel> responseFromFirebase;
+        private static List<SubscriptionToken> subscriptionTokens;
+        private static SendTokensResult result;
+        private static SubscriptionToken notWorkingSubscriptionToken;
+        private static SubscriptionToken workingSubscriptionToken;
+    }
+}
