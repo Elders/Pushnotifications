613baca4ae4dd0de942f7d30ba09b57417cb26f7
joannayankova <joannayankova@gmail.com>
2021-12-07 14:36:12 +0200

    Added  PushNotifications.Contracts  project

---
+++ src/PushNotifications.Api.Reference/PushNotifications.Contracts/PushNotifications/PushNotificationId.cs
@@ -0,0 +1,15 @@
+﻿using Elders.Cronus;
+using System.Runtime.Serialization;
+
+namespace PushNotifications.Contracts.PushNotifications
+{
+    [DataContract(Name = "4b17a6bc-5b7a-4396-9d1c-be8c5dc37912")]
+    public class PushNotificationId : StringTenantId
+    {
+        PushNotificationId() { }
+
+        public PushNotificationId(IUrn urn) : base(urn, "pushnotification") { }
+
+        public PushNotificationId(string id, string tenant) : base(id, "pushnotification", tenant) { }
+    }
+}
---
+++ src/PushNotifications.Api.Reference/PushNotifications.Contracts/Subscriptions/Events/UnSubscribed.cs
@@ -0,0 +1,37 @@
+﻿using Elders.Cronus;
+using System.Runtime.Serialization;
+using System;
+
+namespace PushNotifications.Contracts.Subscriptions.Events
+{
+    [DataContract(Name = "9e655fe8-75d2-4481-a358-aec9d24e9e3d")]
+    public class UnSubscribed : IEvent
+    {
+        UnSubscribed() { }
+
+        public UnSubscribed(SubscriptionId id, SubscriberId subscriberId, SubscriptionToken subscriptionToken)
+        {
+            if (StringTenantId.IsValid(id) == false) throw new ArgumentException(nameof(id));
+            if (StringTenantId.IsValid(subscriberId) == false) throw new ArgumentException(nameof(subscriberId));
+            if (SubscriptionToken.IsValid(subscriptionToken) == false) throw new ArgumentException(nameof(subscriptionToken));
+
+            Id = id;
+            SubscriberId = subscriberId;
+            SubscriptionToken = subscriptionToken;
+        }
+
+        [DataMember(Order = 1)]
+        public SubscriptionId Id { get; private set; }
+
+        [DataMember(Order = 2)]
+        public SubscriberId SubscriberId { get; private set; }
+
+        [DataMember(Order = 3)]
+        public SubscriptionToken SubscriptionToken { get; private set; }
+
+        public override string ToString()
+        {
+            return $"Subscriber '{SubscriberId.Urn.Value}' has unsubscribed with token '{SubscriptionToken}'. Id: '{Id.Urn.Value}' ";
+        }
+    }
+}
