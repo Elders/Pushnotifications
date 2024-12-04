77386ac8d422e99f9f4700f23146569e011167b1
Gavrail Shodov <gavrail.shodov@eldersoss.com>
2024-12-04 12:06:37 +0200

    uip Cleaning dublicated events

---
+++ src/PushNotifications.Api/GavrailDeletesUnsubscribeDublicateController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Elders.Cronus;
+using Microsoft.AspNetCore.Mvc;
+using PushNotifications.Api.Controllers;
+using PushNotifications.Api.Controllers.Subscriptions.Commands;
+using PushNotifications.MigrationSignals;
+
+namespace PushNotifications.Api
+{
+    [Route("RefreshPnEvents")]
+    public class GavrailDeletesUnsubscribeDublicateController : ApiControllerBase
+    {
+        private readonly IPublisher<ISignal> _publisher;
+
+        public GavrailDeletesUnsubscribeDublicateController(IPublisher<ISignal> publisher)
+        {
+            _publisher = publisher;
+        }
+
+        [HttpGet]
+        public IActionResult RefreshPNEvents()
+        {
+            GavrailDeletesUnsubscribeDublicateEventsSignal signal = new GavrailDeletesUnsubscribeDublicateEventsSignal("pruvit", true, DateTimeOffset.UtcNow);
+            _publisher.Publish(signal);
+
+            return Ok("Aideeeeee");
+        }
+    }
+}
